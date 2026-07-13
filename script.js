d9e498e18b1e85685c60da5c048945bfeb84f933
Nsantos45 <165631978+Nsantos45@users.noreply.github.com>
2026-07-12 21:33:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('.card').forEach(card => {
-card.addEventListener('mouseenter', () => {
-card.style.boxShadow = "0 8px 20px rgba(0,0,0,0.15)";
-});
-card.addEventListener('mouseleave', () => {
-card.style.boxShadow = "0 4px 12px rgba(0,0,0,0.08)";
-});
-});
+
